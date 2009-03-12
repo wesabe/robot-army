6286313de0f1cb2ec31fa652ee233bd0a1703b0d
Brian Donovan <brian@wesabe.com>
2009-03-12 15:43:36 -0700

    Really go all out trying to identify things that aren't marshalable.

--- lib/robot-army/marshal_ext.rb
+++ lib/robot-army/marshal_ext.rb
@@ -1,17 +1,17 @@
 module Marshal
   class <<self
     # Determines whether a given object can be dumped.
-    # 
+    #
     # @param object Object
     #   The object to check.
-    # 
+    #
     # @return [Boolean]
-    #   +true+ if dumping the object does not raise an error, 
+    #   +true+ if dumping the object does not raise an error,
     #   +false+ if a +TypeError+ is raised.
-    # 
+    #
     # @raise Exception
     #   Whatever +Marshal.dump+ might raise that isn't a +TypeError+.
-    # 
+    #
     def can_dump?(object)
       begin
         Marshal.dump(object)
@@ -25,9 +25,28 @@ end
 
 class Object
   # Syntactic sugar for +Marshal.can_dump?+.
-  # 
+  #
   # @see Marshal.can_dump?
   def marshalable?
-    Marshal.can_dump?(self)
+    Marshal.can_dump?(self) &&
+    instance_variables.all? do |ivar|
+      instance_variable_get(ivar).marshalable?
+    end
+  end
+end
+
+class Array
+  def marshalable?
+    super &&
+    all? {|item| item.marshalable?}
+  end
+end
+
+class Hash
+  def marshalable?
+    super &&
+    keys.marshalable? &&
+    values.marshalable? &&
+    default.marshalable?
   end
 end
--- spec/marshal_ext_spec.rb
+++ spec/marshal_ext_spec.rb
@@ -1,34 +1,86 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
+class CannotMarshalMe
+  def marshalable?
+    false
+  end
+end
+
+class CustomContainer
+  def initialize(child)
+    @child = child
+  end
+end
+
 describe Marshal do
   it "can dump Fixnums" do
     42.must be_marshalable
   end
-  
+
   it "can dump Strings" do
     "foo".must be_marshalable
   end
-  
+
   it "can dump Arrays" do
     [2, 'foo'].must be_marshalable
   end
-  
+
+  it "can't dump things that return false for marshalable?" do
+    CannotMarshalMe.new.must_not be_marshalable
+  end
+
+  it "can't dump Arrays containing unmarshalable items" do
+    [2, $stdin].must_not be_marshalable
+    [2, CannotMarshalMe.new].must_not be_marshalable
+  end
+
+  it "can dump hashes" do
+    {:foo => 'bar'}.must be_marshalable
+  end
+
+  it "can't dump hashes with unmarshalable keys" do
+    {$stdin => 'bar'}.must_not be_marshalable
+    {CannotMarshalMe.new => 'bar'}.must_not be_marshalable
+  end
+
+  it "can't dump hashes with unmarshalable values" do
+    {:foo => $stdin}.must_not be_marshalable
+    {:foo => CannotMarshalMe.new}.must_not be_marshalable
+  end
+
+  it "can't dump hashes with an unmarshalable default value" do
+    Hash.new($stdin).must_not be_marshalable
+    Hash.new(CannotMarshalMe.new).must_not be_marshalable
+  end
+
+  it "can dump hashes with a marshalable default value" do
+    Hash.new(0).must be_marshalable
+  end
+
+  it "can't dump hashes with a default proc" do
+    Hash.new {|a,b| a[b] = rand}.must_not be_marshalable
+  end
+
+  it "can't dump objects containing references to unmarshalable objects" do
+    CustomContainer.new(CannotMarshalMe.new).must_not be_marshalable
+  end
+
   it "can't dump IOs" do
     $stdin.must_not be_marshalable
   end
-  
+
   it "can't dump Methods" do
     method(:to_s).must_not be_marshalable
   end
-  
+
   it "can't dump bindings" do
     binding.must_not be_marshalable
   end
-  
+
   it "can't dump Procs" do
     proc{ 2 }.must_not be_marshalable
   end
-  
+
   it "can't dump anything whose _dump method raises a TypeError" do
     class NotDumpable; def _dump(*args); raise TypeError; end; end
     NotDumpable.new.must_not be_marshalable
